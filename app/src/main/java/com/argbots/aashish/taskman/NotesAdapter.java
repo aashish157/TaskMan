9de9842e3ecb5988c9d28ff32039ecbc744ef630
Aashish Raj Gupta <argupta157@gmail.com>
2018-10-24 23:51:25 +0530

    whatsapp share update

--- app/src/main/java/com/argbots/aashish/taskman/NotesAdapter.java
+++ app/src/main/java/com/argbots/aashish/taskman/NotesAdapter.java
@@ -2,6 +2,7 @@ package com.argbots.aashish.taskman;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -11,6 +12,14 @@ import android.widget.PopupMenu;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,6 +87,33 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder
                           case R.id.one :   Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
                                             whatsappIntent.setType("text/plain");
                                             whatsappIntent.setPackage("com.whatsapp");
+
+                                            //Retrieve data from firebase
+                                            FirebaseUser u = FirebaseAuth.getInstance().getCurrentUser();
+                                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                            DocumentReference myDocRef = db.collection(u.getEmail()).document(note.getTid());
+
+                                            myDocRef.get()
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+
+                                                  @Override
+                                                  public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                                                  if(documentSnapshot.exists()){
+                                                      String fullname = documentSnapshot.getString("email");
+                                                      String toc = documentSnapshot.getString("toc");
+                                                      String task = documentSnapshot.getString("task");
+                                                  }
+
+                                              }
+                                            })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+
+                                                }
+                                            });
+
                                             whatsappIntent.putExtra(Intent.EXTRA_TEXT, ""+note.getTid());
                                             try {
                                                 mCtx.startActivity(whatsappIntent);
